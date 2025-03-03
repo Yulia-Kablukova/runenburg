04c5f912a7efa1df1d3dfd7e5c8b52416bf0372f
Yulia Kablukova <y.kablukova@alumni.nsu.ru>
2025-03-03 19:10:11 +0700

    add linter, prettier, help and contact

--- src/db/index.js
+++ src/db/index.js
@@ -1,63 +1,81 @@
-import sqlite3 from "sqlite3";
-import { open } from 'sqlite';
+import sqlite3 from 'sqlite3'
+import { open } from 'sqlite'
 
 const db = await open({
-    filename: 'runenburg-db.db',
-    driver: sqlite3.Database
-});
+  filename: 'runenburg-db.db',
+  driver: sqlite3.Database
+})
 
 export async function initDatabase() {
-    await db.exec(`
-        CREATE TABLE IF NOT EXISTS users (
-          id INTEGER PRIMARY KEY,
-          chat_id INTEGER NOT NULL UNIQUE,
-          username TEXT NOT NULL,
-          name TEXT NOT NULL
-        )
-    `);
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS users (
+      id INTEGER PRIMARY KEY,
+      chat_id INTEGER NOT NULL UNIQUE,
+      username TEXT NOT NULL,
+      name TEXT NOT NULL
+    )
+    `)
 
-    await db.exec(`
-        CREATE TABLE IF NOT EXISTS subscriptions (
-          id INTEGER PRIMARY KEY AUTOINCREMENT,
-          chat_id INTEGER NOT NULL,
-          sex INTEGER NOT NULL,
-          brand TEXT NOT NULL,
-          size TEXT NOT NULL,
-          UNIQUE (chat_id, sex, brand, size)
-        )
-    `);
+  await db.exec(`
+    CREATE TABLE IF NOT EXISTS subscriptions (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      chat_id INTEGER NOT NULL,
+      sex INTEGER NOT NULL,
+      brand TEXT NOT NULL,
+      size TEXT NOT NULL,
+      UNIQUE (chat_id, sex, brand, size)
+    )
+    `)
 }
 
 export async function createUser(id, chat_id, username, name) {
-    await db.run('INSERT OR IGNORE INTO users (id, chat_id, username, name) VALUES (?, ?, ?, ?)', id, chat_id, username, name);
+  await db.run(
+    'INSERT OR IGNORE INTO users (id, chat_id, username, name) VALUES (?, ?, ?, ?)',
+    id,
+    chat_id,
+    username,
+    name
+  )
 }
 
 export async function getUsers() {
-    return await db.all('SELECT username, name FROM users');
+  return await db.all('SELECT username, name FROM users')
 }
 
 export async function createSubscription(chat_id, sex, brand, size) {
-    await db.run('INSERT OR IGNORE INTO subscriptions (chat_id, sex, brand, size) VALUES (?, ?, ?, ?)', chat_id, sex, brand, size);
+  await db.run(
+    'INSERT OR IGNORE INTO subscriptions (chat_id, sex, brand, size) VALUES (?, ?, ?, ?)',
+    chat_id,
+    sex,
+    brand,
+    size
+  )
 }
 
 export async function getSubscriptions() {
-    return await db.all('SELECT * FROM subscriptions');
+  return await db.all('SELECT * FROM subscriptions')
 }
 
-export async function getFilteredSubscriptionUsers(sex, brands, sizes) {
-    return await db.all(
-        `SELECT chat_id FROM subscriptions WHERE sex = ? AND brand IN (${brands.map(() => '?').join(',')}) AND size IN (${sizes.map(() => '?').join(',')})`,
-        sex, ...brands, ...sizes);
+export async function getSubscriptionChats(sex, brands, sizes) {
+  return await db.all(
+    `SELECT chat_id FROM subscriptions WHERE sex = ? AND brand IN (${brands.map(() => '?').join(',')}) AND size IN (${sizes.map(() => '?').join(',')})`,
+    sex,
+    ...brands,
+    ...sizes
+  )
 }
 
-export async function getUserSubscriptions(chat_id) {
-    return await db.all('SELECT * FROM subscriptions WHERE chat_id = ? ORDER BY id', chat_id);
+export async function getChatSubscriptions(chat_id) {
+  return await db.all(
+    'SELECT * FROM subscriptions WHERE chat_id = ? ORDER BY id',
+    chat_id
+  )
 }
 
 export async function deleteSubscription(id) {
-    await db.run('DELETE FROM subscriptions WHERE id = ?', id);
+  await db.run('DELETE FROM subscriptions WHERE id = ?', id)
 }
 
-export async function deleteUserSubscriptions(chat_id) {
-    await db.run('DELETE FROM subscriptions WHERE chat_id = ?', chat_id);
+export async function deleteChatSubscriptions(chat_id) {
+  await db.run('DELETE FROM subscriptions WHERE chat_id = ?', chat_id)
 }
--- src/keyboards/index.js
+++ src/keyboards/index.js
@@ -1,80 +1,94 @@
-import { InlineKeyboard } from 'grammy';
+import { InlineKeyboard } from 'grammy'
 
 export const brands = [
-    { label: 'Nike', data: 'nike' },
-    { label: 'Adidas', data: 'adidas' },
-    { label: 'Asics', data: 'asics' },
-    { label: 'New Balance', data: 'new_balance' },
-    { label: 'Saucony', data: 'saucony' },
-    { label: 'Brooks', data: 'brooks' },
-    { label: 'Hoka', data: 'hoka' },
-    { label: 'Puma', data: 'puma' },
-    { label: 'Mizuno', data: 'mizuno' },
-    { label: 'On Running', data: 'on_running' },
-    { label: 'Salomon', data: 'salomon' },
-    { label: 'Altra', data: 'altra' },
-    { label: 'Anta', data: 'anta' },
-    { label: 'Skechers', data: 'skechers' },
-    { label: 'Bmai', data: 'bmai' },
-    { label: 'Li-Ning', data: 'li_ning' },
+  { label: 'Nike', data: 'nike' },
+  { label: 'Adidas', data: 'adidas' },
+  { label: 'Asics', data: 'asics' },
+  { label: 'New Balance', data: 'new_balance' },
+  { label: 'Saucony', data: 'saucony' },
+  { label: 'Brooks', data: 'brooks' },
+  { label: 'Hoka', data: 'hoka' },
+  { label: 'Puma', data: 'puma' },
+  { label: 'Mizuno', data: 'mizuno' },
+  { label: 'On Running', data: 'on_running' },
+  { label: 'Salomon', data: 'salomon' },
+  { label: 'Altra', data: 'altra' },
+  { label: 'Anta', data: 'anta' },
+  { label: 'Skechers', data: 'skechers' },
+  { label: 'Bmai', data: 'bmai' },
+  { label: 'Li-Ning', data: 'li_ning' }
 ]
 
 export const sexes = [
-    { label: 'Мужской', data: 'male' },
-    { label: 'Женский', data: 'female' },
+  { label: 'Мужской', data: 'male' },
+  { label: 'Женский', data: 'female' }
 ]
 
 export const sizes = [
-    { label: '22,5', data: 'size_22_5' },
-    { label: '23', data: 'size_23' },
-    { label: '23,5', data: 'size_23_5' },
-    { label: '24', data: 'size_24' },
-    { label: '24,5', data: 'size_24_5' },
-    { label: '25', data: 'size_25' },
-    { label: '25,25', data: 'size_25_25' },
-    { label: '25,5', data: 'size_25_5' },
-    { label: '25,75', data: 'size_25_75' },
-    { label: '26', data: 'size_26' },
-    { label: '26,5', data: 'size_26_5' },
-    { label: '27', data: 'size_27' },
-    { label: '27,5', data: 'size_27_5' },
-    { label: '28', data: 'size_28' },
-    { label: '28,25', data: 'size_28_25' },
-    { label: '28,5', data: 'size_28_5' },
-    { label: '29', data: 'size_29' },
-    { label: '29,5', data: 'size_29_5' },
-    { label: '30', data: 'size_30' },
-    { label: '30,5', data: 'size_30_5' },
-    { label: '31', data: 'size_31' },
-    { label: '31,5', data: 'size_31_5' },
-    { label: '32', data: 'size_32' },
-    { label: '32,5', data: 'size_32_5' },
-    { label: '33', data: 'size_33' },
-    { label: '33,5', data: 'size_33_5' },
-    { label: '34', data: 'size_34' },
-    { label: '34,5', data: 'size_34_5' },
-    { label: '35', data: 'size_35' },
+  { label: '22,5', data: 'size_22_5' },
+  { label: '23', data: 'size_23' },
+  { label: '23,5', data: 'size_23_5' },
+  { label: '24', data: 'size_24' },
+  { label: '24,5', data: 'size_24_5' },
+  { label: '25', data: 'size_25' },
+  { label: '25,25', data: 'size_25_25' },
+  { label: '25,5', data: 'size_25_5' },
+  { label: '25,75', data: 'size_25_75' },
+  { label: '26', data: 'size_26' },
+  { label: '26,5', data: 'size_26_5' },
+  { label: '27', data: 'size_27' },
+  { label: '27,5', data: 'size_27_5' },
+  { label: '28', data: 'size_28' },
+  { label: '28,25', data: 'size_28_25' },
+  { label: '28,5', data: 'size_28_5' },
+  { label: '29', data: 'size_29' },
+  { label: '29,5', data: 'size_29_5' },
+  { label: '30', data: 'size_30' },
+  { label: '30,5', data: 'size_30_5' },
+  { label: '31', data: 'size_31' },
+  { label: '31,5', data: 'size_31_5' },
+  { label: '32', data: 'size_32' },
+  { label: '32,5', data: 'size_32_5' },
+  { label: '33', data: 'size_33' },
+  { label: '33,5', data: 'size_33_5' },
+  { label: '34', data: 'size_34' },
+  { label: '34,5', data: 'size_34_5' },
+  { label: '35', data: 'size_35' }
 ]
 
-export const brandsKeyboard = brands.reduce((result, { label, data }, index) => {
+export const brandsKeyboard = brands.reduce(
+  (result, { label, data }, index) => {
     if (index && !(index % 2)) {
-        result.row()
+      result.row()
     }
     return result.text(label, data)
-}, new InlineKeyboard())
+  },
+  new InlineKeyboard()
+)
 
-export const sexKeyboard = sexes.reduce((result, { label, data }) => result.text(label, data).row(), new InlineKeyboard())
+export const sexKeyboard = sexes.reduce(
+  (result, { label, data }) => result.text(label, data).row(),
+  new InlineKeyboard()
+)
 
 export const sizeKeyboard = sizes.reduce((result, { label, data }, index) => {
-    if (index && !(index % 3)) {
-        result.row()
-    }
-    return result.text(label, data)
+  if (index && !(index % 3)) {
+    result.row()
+  }
+  return result.text(label, data)
 }, new InlineKeyboard())
 
-export const confirmKeyboard = new InlineKeyboard().text('Подтвердить', 'confirm');
+export const confirmKeyboard = new InlineKeyboard().text(
+  'Подтвердить',
+  'confirm'
+)
 
 export const postKeyboard = new InlineKeyboard()
-    .text('Изменить сообщение', 'clear_message')
-    .row()
-    .text('Отправить рассылку', 'send');
+  .text('Изменить сообщение', 'clear_message')
+  .row()
+  .text('Отправить рассылку', 'send')
+
+export const contactKeyboard = new InlineKeyboard().url(
+  'Купить',
+  'https://t.me/temchik05'
+)
